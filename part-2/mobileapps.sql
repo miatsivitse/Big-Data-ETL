7d92b9f9617293dd4a1c53d36b7adc3e0a5d40b1
miatsivitse <galluccimia.com>
2023-02-26 19:09:40 -0500

    add part 2 file

---
+++ part-2/mobileapps.sql
@@ -0,0 +1,11 @@
+SELECT * FROM customers_mobileapps WHERE (count > 100)
+
+SELECT AVG(count)
+FROM customers_mobileapps;
+
+SELECT COUNT(count)
+FROM customers_mobileapps
+WHERE (COUNT > 100);
+
+SELECT AVG(star_rating)
+FROM vine_mobileapps;
